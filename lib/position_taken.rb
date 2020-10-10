d07f5b8f25c1ad277c19495898d19b66f6394c08
Jakob Persons <jakobep24@gmail.com>
2020-10-10 18:33:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 def position_taken?(board, index)
-  if !(board[index] == " " || board[index] == "" || board[index] == nil)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
   end
 end
